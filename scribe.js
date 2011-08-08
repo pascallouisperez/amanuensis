a148d772990d7599ca1ed3058897fa133f686978
Pascal-Louis Perez <pascallouisperez@gmail.com>
2011-08-08 14:13:27 -0700

    Initial implementation: stores include console, file, relay, and buffering. The display store is an internal store which keeps last 10 messages in memory and is used by the server to server the /live page.

---
+++ scribe.js
@@ -0,0 +1,54 @@
+var http = require('http');
+var stores = require('./stores');
+
+var display_store = new stores.DisplayStore();
+
+// downstream stores (from args)
+var downstream_stores = (function(configurations) {
+	var s = configurations.map(function (conf) {
+		return object2store(JSON.parse(conf));
+	});
+	s.push(display_store);
+	return s;
+})(process.argv.slice(2));
+
+function object2store(object) {
+	switch (object.type) {
+		case 'console': return new stores.ConsoleStore();
+		case 'file': return new stores.FileStore(object.path);
+		case 'relay': return new stores.RelayStore(object.host);
+		case 'buffering': return new stores.BufferingStore(object.max, object2store(object.delegate));
+		default: throw new Error("unknown type " + object.type);
+	}
+}
+
+// server
+var server = http.createServer(function (req, res) {
+	if (req.method == 'POST' && req.url == '/') {
+		req.on('data', function(chunk) {
+			var message = chunk.toString('utf8');
+			res.writeHead(200, {'Content-Type': 'text/plain'});
+			res.end('OK ' + message.length + '\n');
+			for (var i in downstream_stores) {
+				try {
+					downstream_stores[i].push(message.split('\n'));
+				} catch (e) {
+					console.log(e);
+				}
+			}
+		});
+	} else if (req.method == 'POST' && req.url == '/close') {
+		server.close();
+	} else if (req.method == 'GET' && req.url == '/live') {
+		res.writeHead(200, {'Content-Type': 'text/plain'});
+		for (var i in display_store.queue) {
+			res.write(display_store.queue[i]);
+			res.write('\n');
+		}
+		res.end();
+	} else {
+		res.writeHead(200, {'Content-Type': 'text/plain'});
+		res.end('POST your log message\n');
+	}
+}).listen(50000);
+console.log('Server running');
