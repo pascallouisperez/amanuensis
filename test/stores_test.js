7a7ab7509d62a7579c6acfc958178517f2fbe86f
Pascal-Louis Perez <pascallouisperez@gmail.com>
2011-08-10 16:49:06 -0700

    more testing

--- test/stores_test.js
+++ test/stores_test.js
@@ -4,6 +4,15 @@ var stores = require('stores');
 exports.displayStore_init = function() {
 	var s = new stores.DisplayStore();
 	assert.equal(0, s.queue.length);
+	
 	s.push(['foo', 'bar']);
 	assert.equal(2, s.queue.length);
+	
+	s.push(['baz']);
+	assert.equal(3, s.queue.length);
+	
+	s.push(['msg 4', 'msg 5', 'msg 6', 'msg 7', 'msg 8', 'msg 9', 'msg 10', 'msg 11', 'msg 12']);
+	assert.equal(10, s.queue.length);
+	assert.equal('baz', s.queue[0]);
+	assert.equal('msg 12', s.queue[9]);
 }
